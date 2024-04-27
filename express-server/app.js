b2f5f39819b64032ac1024cd9345cd7632f9bf0c
anturchin <tur4intwo@gmail.com>
2024-04-27 21:16:58 +0300

    feat: add handlebars

--- express-server/app.js
+++ express-server/app.js
@@ -1,31 +1,29 @@
 const express = require('express');
+const { engine: expressHandlebars } = require('express-handlebars');
 
 const app = express();
+
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main',
+}))
+
+app.set('view engine', 'handlebars');
 
 const port = process.env.PORT || 3000;
 
 
-app.get('/', (req, res) => {
-    res.type('text/plain');
-    res.send('home page');
-})
-
-app.get('/about', (req, res) => {
-    res.type('text/plain');
-    res.send('about');
-})
+app.get('/', (req, res) => res.render('home'));
+app.get('/about', (req, res) => res.render('about'));
 
 app.use((req, res) => {
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - not found');
+    res.render('404');
 })
 
 app.use((err, req, res, next) => {
     console.error(err.message);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - error server');
+    res.render('500');
 })
 
 app.listen(port, () => console.log(`server express is running on port ${port}...`));
